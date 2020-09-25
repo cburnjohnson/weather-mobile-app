6c91aec499877c67461d56d27b68b556f887fab3
Cole Johnson <cburnjohnson@gmail.com>
2020-09-24 19:18:05 -0700

    initial weather display

---
+++ components/UnitsPIcker.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Picker } from '@react-native-community/picker';
+
+export default function UnitsPIcker() {
+    return (
+        <View>
+            <Picker>
+                <Picker.Item />
+            </Picker>
+        </View>
+    );
+}
